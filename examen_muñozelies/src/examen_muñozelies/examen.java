faeaecfcc3bba67e4a9b99c0541d93ef0db1acca
marc <losmetineros2@gmail.com>
2018-02-27 09:48:04 +0100

    Examen Marc Muñoz Elies

---
+++ examen_muñozelies/src/examen_muñozelies/examen.java
@@ -0,0 +1,17 @@
+package examen_muñozelies;
+
+public class examen {
+
+	public static void main (String[] args) {
+		printOne();
+		printOne();
+		printTwo();
+	}
+	public static void printOne() {
+		System.out.println("Hello World");
+	}
+	public static void printTwo() {
+		printOne();
+		printOne();
+	}
+}
